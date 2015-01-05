c1fc0c24782bd7a888c212a46e23cdc6b4931480
Vladimir Burdukov <chippcheg@gmail.com>
2015-01-05 20:00:56 +0300

    refactor matrix

--- test.m
+++ test.m
@@ -1,34 +1,36 @@
 L_A1 = 28 * 28;
-L_A2 = 10;
+L_A2 = 25;
 L_A3 = 10;
 
-z2 = X * Theta1;
+z2 = Theta1 * X;
 a2 = sigmoid(z2);
-a2 = [ones(size(a2,1),1) a2];
+a2 = [ones(1,size(a2,2)); a2];
 
-z3 = a2 * Theta2;
+z3 = Theta2 * a2;
 a3 = sigmoid(z3);
 
-m = size(X, 1);
+m = size(X, 2);
 
 J = 0;
 for i = 1:m,  
-    J += sum(-1 * y(i,:) .* log(a3(i,:)) - (1 - y(i,:)) .* log(1 - a3(i,:)));
+    J += sum(-1 * y(:,i) .* log(a3(:,i)) - (1 - y(:,i)) .* log(1 - a3(:,i)));
 end;
 
 J = J / m;
 
 lambda = 1;
-J += lambda / (2 * m) * (sum(sum(Theta1(2:end,:) .^ 2)) + sum(sum(Theta2(2:end,:) .^ 2)));
+J += lambda / (2 * m) * (sum(sum(Theta1(:,2:end) .^ 2)) + sum(sum(Theta2(:,2:end) .^ 2)));
 
 Delta1 = zeros(size(Theta1));
 Delta2 = zeros(size(Theta2));
 
-_delta3 = a3 - y;
+delta3_ = a3 - y;
+Theta2' * delta3_
+% delta2_ = (Theta2' * delta3_)(2:end,:) .* sigmoidGradient(z2)
 
-for i = 1:1,
-    delta3 = _delta3(i,:);
-	delta2 = (Theta2 * delta3')(2:end,:) .* sigmoidGradient(z2(:,i));
+for i = 2:2,
+    delta3 = delta3_(:,i);
+	delta2 = (Theta2' * delta3)(2:end,:) .* sigmoidGradient(z2(:,i))
 
 	% Delta1 += (delta2 * X(i,:))';
 	% Delta2 += (delta3' * a2(i,:))';
